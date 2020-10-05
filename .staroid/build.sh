450fe0939ddc81ae0073e555e39b1cb3a93a7c19
Lee moon soo <leemoonsoo@gmail.com>
2020-10-05 08:59:52 -0700

    Ray on staroid (#2)
    * build staroid image
    
    * make github action build
    
    * fix indent
    
    * github action to build each version in parallel
    
    * parallel build
    
    * fix
    
    * missing $
    
    * set push image false
    
    * try concurrency 1
    
    * bumpup ray version
    
    * use resources.limits and tmp volumes
    
    * build src from open-datastudio/ray
    
    * dedicated only

---
+++ .staroid/build.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+# Custom builder script for Skaffold
+# https://skaffold.dev/docs/pipeline-stages/builders/custom/
+#
+
+set -x
+set -e
+pwd
+
+#RAY_REPO=https://github.com/ray-project/ray.git
+#RAY_CHECKOUT=c17169dc110ed0699eefab04ca17537eb68ce713
+RAY_REPO=https://github.com/open-datastudio/ray.git
+RAY_CHECKOUT=master-staroid-button
+PYTHON_VERSION=$1
+SHORT_VER=`echo $PYTHON_VERSION | sed "s/\([0-9]*\)[.]\([0-9]*\)[.][0-9]*/\1\2/g"`
+
+echo "PYTHON_VERSION=$PYTHON_VERSION, SHORT_VER=$SHORT_VER"
+
+# Checkout ray source
+git clone $RAY_REPO ray-py$SHORT_VER
+cd ray-py$SHORT_VER
+git checkout $RAY_CHECKOUT
+
+# true to build .whl from source (will take about 3 hours).
+# false to use pre-built whl file from http(s) url.
+BUILD_WHEEL=${BUILD_WHEEL:-true}
+
+if [ "$BUILD_WHEEL" == "true" ]; then
+    if [ ! -d ".whl" ]; then # check if already built.
+        git config user.name "build"
+        git config user.email "ci@build.com"
+
+        # patch default ray serve bind address from 127.0.0.1 to 0.0.0.0
+        sed -i "s/DEFAULT_HTTP_HOST = \"127.0.0.1\"/DEFAULT_HTTP_HOST = \"0.0.0.0\"/g" python/ray/serve/constants.py
+        git commit python/ray/serve/constants.py -m "patch serve bind address"
+
+        # increase timeout for 'ray up' command
+        sed -i "s/NODE_START_WAIT_S = 300/NODE_START_WAIT_S = 1200/g" python/ray/autoscaler/_private/command_runner.py
+        git commit python/ray/autoscaler/_private/command_runner.py -m "increase ray up timeout"
+
+        # Uncomment followings to build wheel for only single python version.
+        if [ "BUILD_WHEEL_SINGLE_VERSION" == "true" ]; then
+            if [ "$SHORT_VER" == "36" ]; then
+                WHL_STRING="cp36-cp36m"
+            elif [ "$SHORT_VER" == "37" ]; then
+                WHL_STRING="cp37-cp37m"
+            elif [ "$SHORT_VER" == "38" ]; then
+                WHL_STRING="cp38-cp38"
+            fi
+            sed -ie "/^PYTHONS=/,+2d" python/build-wheel-manylinux1.sh
+            sed -ie "/^chmod/a PYTHONS=\(\"$WHL_STRING\"\)" python/build-wheel-manylinux1.sh
+            git commit python/build-wheel-manylinux1.sh -m "update"
+            cat python/build-wheel-manylinux1.sh
+        fi
+
+        # current commit
+        COMMIT=`git rev-parse HEAD`
+
+        docker run \
+            -e TRAVIS_COMMIT=$COMMIT \
+            --rm -i \
+            -w /ray \
+            -v `pwd`:/ray \
+            rayproject/arrow_linux_x86_64_base:python-3.8.0 \
+            /ray/python/build-wheel-manylinux1.sh
+    fi
+
+    WHEEL=`ls .whl/*-cp$SHORT_VER-*`
+else
+    if [ "$SHORT_VER" == "36" ]; then
+        WHEEL="https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-1.1.0.dev0-cp36-cp36m-manylinux1_x86_64.whl"
+    elif [ "$SHORT_VER" == "37" ]; then
+        WHEEL="https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-1.1.0.dev0-cp37-cp37m-manylinux1_x86_64.whl"
+    elif [ "$SHORT_VER" == "38" ]; then
+        WHEEL="https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-1.1.0.dev0-cp38-cp38-manylinux1_x86_64.whl"
+    fi
+fi
+
+# apply non-root docker image patch
+../.staroid/ray_patch.sh reset . .
+../.staroid/ray_patch.sh patch . $WHEEL
+
+# apply additional docker file commands
+cat ../.staroid/Dockerfile_staroid >> docker/ray-ml/Dockerfile
+
+# print patched files
+git diff
+
+cat docker/base-deps/Dockerfile
+cat docker/ray-deps/Dockerfile
+cat docker/ray/Dockerfile
+cat docker/ray-ml/Dockerfile
+
+# cp requirements to ray-ml dir
+cp python/requirements* docker/ray-ml
+
+# build docker image
+./build-docker.sh --no-cache-build --gpu --python-version $PYTHON_VERSION
+
+# print images
+docker tag rayproject/ray-ml:latest-gpu $IMAGE
+docker images
+
+# verify image hash are all different.
+# In case of a parent image build fail,
+# 'FROM ...' command child image will pull from internet instead of using local build,
+# and child image bulid will success without error.
+# In this case, multiple image may end up with the same image hash.
+UNIQ_IMAGES=`docker images | grep ray-py | awk '{print $3}' | uniq | wc -l`
+NUM_IMAGES=`docker images | grep ray-py | awk '{print $3}' | wc -l`
+
+if [ "$NUM_IMAGES" != "$UNIQ_IMAGES" ]; then
+    echo "Error. Duplicated image hash found."
+    exit 1
+fi
+
+if $PUSH_IMAGE; then
+    docker push $IMAGE
+fi
--- .staroid/ray_patch.sh
+++ .staroid/ray_patch.sh
@@ -3,15 +3,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ $# -ne 3 ]; then
-    echo "usage) $0 [patch|reset] [RAY_HOME] [PYTHON_VERSION]"
+    echo "usage) $0 [patch|reset] [RAY_HOME] [WHEEL]"
     exit 1
 fi
 
 OP=$1
 RAY_HOME=$2
-PYTHON_VERSION=$3
+WHEEL=$3
 
-RAY_WHEEL_URL='https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-1.1.0.dev0-cp${PY_VER}-cp${PY_VER}m-manylinux1_x86_64.whl'
 RAY_UID=1000
 RAY_GID=100
 
@@ -22,14 +21,20 @@ if [ $? -eq 0 ]; then
 fi
 
 if [ "$OP" == "patch" ]; then
-    # patch miniconda python version
-    $SED_INPLACE "s/python=3.7.7/python=$PYTHON_VERSION/g" $RAY_HOME/docker/base-deps/Dockerfile
-
     # patch wheel url
-    PY_VER=`echo $PYTHON_VERSION | sed 's/\([0-9]\)[.]\([0-9]\).*/\1\2/g'`
-    RAY_WHEEL_URL=`eval echo "$RAY_WHEEL_URL"`
-    $SED_INPLACE "s|WHEEL_URL=.*.whl\"|WHEEL_URL=\"${RAY_WHEEL_URL}\"|g" $RAY_HOME/build-docker.sh
-    $SED_INPLACE "s|set -x|set -x; set -e|g" $RAY_HOME/build-docker.sh
+    echo "$WHEEL" | grep ^http > /dev/null
+    if [ $? -ne 0 ]; then
+        # path is given
+        $SED_INPLACE "s|set -x|set -x; set -e|g" $RAY_HOME/build-docker.sh
+        $SED_INPLACE "s|^WHEEL=.*|WHEEL=$WHEEL|g" $RAY_HOME/build-docker.sh
+        $SED_INPLACE "s|wget.*||g" $RAY_HOME/build-docker.sh
+    else
+        # url is given
+        $SED_INPLACE "s|^WHEEL_URL=.*|WHEEL_URL=\"$WHEEL\"|g" $RAY_HOME/build-docker.sh
+    fi
+
+    # build ray-ml
+    $SED_INPLACE "s/\"ray-deps\" \"ray\"/\"ray-deps\" \"ray\" \"ray-ml\"/g" $RAY_HOME/build-docker.sh
 
     # patch PATH
     $SED_INPLACE "s/\/root/\/home\/ray/g" ${RAY_HOME}/docker/base-deps/Dockerfile
@@ -48,8 +53,9 @@ if [ "$OP" == "patch" ]; then
     $SED_INPLACE "s/apt-get/sudo apt-get/g" ${RAY_HOME}/docker/ray-ml/Dockerfile
 
     # patch rm
-    $SED_INPLACE "s/ rm /sudo rm /g" ${RAY_HOME}/docker/ray-deps/Dockerfile
-    $SED_INPLACE "s/ rm /sudo rm /g" ${RAY_HOME}/docker/ray/Dockerfile
+    $SED_INPLACE "s/ rm / sudo rm /g" ${RAY_HOME}/docker/ray-deps/Dockerfile
+    $SED_INPLACE "s/ rm / sudo rm /g" ${RAY_HOME}/docker/ray/Dockerfile
+    $SED_INPLACE "s/ rm / sudo rm /g" ${RAY_HOME}/docker/ray-ml/Dockerfile
 
     # Add ray user & install sudo
     # lines until 'ARG DEBIAN_FRONTNED ...'
@@ -67,17 +73,44 @@ RUN useradd -ms /bin/bash -d /home/ray ray --uid $RAY_UID --gid $RAY_GID \
 USER 1000
 ENV HOME=/home/ray
 EOF
+
     # lines after 'ARG DEBIAN_FRONTNED ...'
     cat $RAY_HOME/docker/base-deps/Dockerfile | sed '1,/ARG DEBIAN/d' >> /tmp/ray_tmp_docker
     mv /tmp/ray_tmp_docker $RAY_HOME/docker/base-deps/Dockerfile
+
+    # in case of py38, atari-py package installation fails without few os packages
+    $SED_INPLACE "s/RUN \$HOME/RUN sudo apt-get update \&\& sudo apt-get install -y g++ cmake zlib1g-dev \&\& \$HOME/g" ${RAY_HOME}/docker/ray-deps/Dockerfile
+    $SED_INPLACE "s/RUN \$HOME/RUN sudo apt-get update \&\& sudo apt-get install -y g++ cmake zlib1g-dev \&\& \$HOME/g" ${RAY_HOME}/docker/ray/Dockerfile
+    $SED_INPLACE "s/\(\&\& sudo rm.*\)/\1 \&\& sudo apt-get autoremove -y cmake g++ \&\& sudo rm -rf \/var\/lib\/apt\/lists\/\* \&\& sudo apt-get clean/g" ${RAY_HOME}/docker/ray-deps/Dockerfile
+    $SED_INPLACE "s/\(\&\& sudo rm.*\)/    \1 \&\& sudo apt-get autoremove -y cmake g++ \&\& sudo rm -rf \/var\/lib\/apt\/lists\/\* \&\& sudo apt-get clean/g" ${RAY_HOME}/docker/ray/Dockerfile
+
+    # remove tensorflow from pip.
+    # tensorflow need to be installed using conda, because
+    # tensorflow is currently built for cuda 10.1
+    # and base nvidia-docker image has cuda 11.0 installed.
+    #
+    # Although Dockerfile_staroid sym link cuda 10.1 from cuda 11.0 files (cuda 11 supposed to backward compatible),
+    # tensorflow detect GPU correctly but crashing actual code in this way.
+    #
+    # conda install cuda 10.1 binaries together with tensorflow and it works well.
+    # Therefore Dockerfile_staroid will install tensorflow using conda.
+    $SED_INPLACE "/tensorflow/d" ${RAY_HOME}/python/requirements.txt
+    $SED_INPLACE "/tensorflow/d" ${RAY_HOME}/python/requirements_ml_docker.txt
+    $SED_INPLACE "/tensorflow/d" ${RAY_HOME}/python/requirements_rllib.txt
+    $SED_INPLACE "/tensorflow/d" ${RAY_HOME}/python/requirements_tune.txt
+
 elif [ "$OP" == "reset" ]; then
     git checkout ${RAY_HOME}/docker/ray/Dockerfile
     git checkout ${RAY_HOME}/docker/ray-deps/Dockerfile
     git checkout ${RAY_HOME}/docker/base-deps/Dockerfile
     git checkout ${RAY_HOME}/docker/ray-ml/Dockerfile
     git checkout ${RAY_HOME}/build-docker.sh
+
+    git checkout ${RAY_HOME}/python/requirements.txt
+    git checkout ${RAY_HOME}/python/requirements_ml_docker.txt
+    git checkout ${RAY_HOME}/python/requirements_rllib.txt
+    git checkout ${RAY_HOME}/python/requirements_tune.txt
 else
     echo "Invalid operation $OP"
     exit 1
 fi
-
