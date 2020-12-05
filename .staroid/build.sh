b8e9498bf6ed72c877c5b04989e0a80599edb8dc
Lee moon soo <leemoonsoo@gmail.com>
2020-12-05 13:12:25 -0800

    update ray commit hash

--- .staroid/build.sh
+++ .staroid/build.sh
@@ -8,7 +8,7 @@ set -e
 pwd
 
 RAY_REPO=https://github.com/ray-project/ray.git
-RAY_CHECKOUT=643c07e451f003fdce5f9dae4d32ade41a4a53c3
+RAY_CHECKOUT=260b07cf0cf2c10c091711cc3d598663133c2dc5
 PYTHON_VERSION=$1
 SHORT_VER=`echo $PYTHON_VERSION | sed "s/\([0-9]*\)[.]\([0-9]*\)[.][0-9]*/\1\2/g"`
 
