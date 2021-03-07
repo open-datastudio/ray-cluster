be435fa92bc98f3b37d4ee72e06f3b6a990c3aa2
Lee moon soo <leemoonsoo@gmail.com>
2021-03-07 15:09:48 -0800

    define SED_INPLACE

--- .staroid/build.sh
+++ .staroid/build.sh
@@ -14,6 +14,13 @@ SHORT_VER=`echo $PYTHON_VERSION | sed "s/\([0-9]*\)[.]\([0-9]*\)[.][0-9]*/\1\2/g
 
 echo "PYTHON_VERSION=$PYTHON_VERSION, SHORT_VER=$SHORT_VER"
 
+# sed
+SED_INPLACE="sed -i"
+uname | grep Darwin > /dev/null
+if [ $? -eq 0 ]; then
+    SED_INPLACE="sed -i .bak"
+fi
+
 # Checkout ray source
 git clone $RAY_REPO ray-py$SHORT_VER
 cd ray-py$SHORT_VER
